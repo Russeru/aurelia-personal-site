ae63f2ec515d9987ba94c6eb54a56389eb6abebd
Me 254925 <254925@russeru.nfshost.com>
2017-04-21 01:18:17 +0000

    Update app.js with new homepage name

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     // config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Home' },
+      { route: ['', 'home'], name: 'home',      moduleId: 'home',      nav: true, title: 'Home' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Projects' }
       // { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
     ]);
