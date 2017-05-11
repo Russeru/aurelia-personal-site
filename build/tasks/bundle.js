49d81b66700c1022bc488e99f625c9744ce81709
Walker, Russell E <russell.walker@optum.com>
2017-05-11 00:24:23 -0400

    Bundling fix

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,57 +1,16 @@
 var gulp = require('gulp');
 var bundler = require('aurelia-bundler');
-// var bundles = require('../bundles.js');
-var bundle = require('aurelia-bundler').bundle;
+var bundles = require('../bundles.js');
 
 var config = {
   force: true,
-  baseURL: '.',                   // baseURL of the application
-  configPath: './config.js',      // config.js file. Must be within `baseURL`
-  "bundles": {
-    "dist/app-build": {
-      "includes": [
-        "[**/*.js]",
-        "**/*.html!text",
-        "**/*.css!text"
-      ],
-      "options": {
-        "inject": true,
-        "minify": true,
-        "depCache": true,
-        "rev": false
-      }
-    },
-    "dist/aurelia": {
-      "includes": [
-        "aurelia-framework",
-        "aurelia-bootstrapper",
-        "aurelia-fetch-client",
-        "aurelia-router",
-        "aurelia-animator-css",
-        "aurelia-templating-binding",
-        "aurelia-polyfills",
-        "aurelia-templating-resources",
-        "aurelia-templating-router",
-        "aurelia-loader-default",
-        "aurelia-history-browser",
-        "aurelia-logging-console",
-        "bootstrap",
-        "bootstrap/css/bootstrap.css!text",
-        "fetch",
-        "jquery"
-      ],
-      "options": {
-        "inject": true,
-        "minify": true,
-        "depCache": false,
-        "rev": false
-      }
-    }
-  }
+  baseURL: '.',
+  configPath: './config.js',
+  bundles: bundles.bundles
 };
 
-gulp.task('bundle', function() {
-  return bundle(config);
+gulp.task('bundle', ['build'], function() {
+  return bundler.bundle(config);
 });
 
 gulp.task('unbundle', function() {
