bbdb030df2e366e8ecd9af7d417a6b963bcfb6f3
Walker, Russell E <russell.walker@optum.com>
2017-05-10 23:51:30 -0400

    Hopefully better bundling

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,41 +1,50 @@
 var gulp = require('gulp');
 var bundler = require('aurelia-bundler');
-var bundles = require('../bundles.js');
+// var bundles = require('../bundles.js');
 var bundle = require('aurelia-bundler').bundle;
 
 var config = {
   force: true,
   baseURL: '.',                   // baseURL of the application
   configPath: './config.js',      // config.js file. Must be within `baseURL`
-  bundles: {
-    "dist/app-build": {           // bundle name/path. Must be within `baseURL`. Final path is: `baseURL/dist/app-build.js`.
-      includes: [
-        '[*.js]',
-        '*.html!text',
-        '*.css!text'        
+  "bundles": {
+    "dist/app-build": {
+      "includes": [
+        "[**/*.js]",
+        "**/*.html!text",
+        "**/*.css!text"
       ],
-      options: {
-        inject: true,
-        minify: true
+      "options": {
+        "inject": true,
+        "minify": true,
+        "depCache": true,
+        "rev": false
       }
     },
-    "dist/vendor-build": {
-      includes: [
-        'aurelia-bootstrapper',
-        'aurelia-fetch-client',
-        'aurelia-router',
-        'aurelia-animator-css',
-        'npm:aurelia-templating-binding@1.3.0',
-        'npm:aurelia-templating-resources@1.4.0',
-        'npm:aurelia-templating-router@1.1.0',
-        'npm:aurelia-loader-default@1.0.2',
-        'npm:aurelia-history-browser@1.0.0',
-        'npm:aurelia-logging-console@1.0.0',
-        'bootstrap/css/bootstrap.css!text'
+    "dist/aurelia": {
+      "includes": [
+        "aurelia-framework",
+        "aurelia-bootstrapper",
+        "aurelia-fetch-client",
+        "aurelia-router",
+        "aurelia-animator-css",
+        "aurelia-templating-binding",
+        "aurelia-polyfills",
+        "aurelia-templating-resources",
+        "aurelia-templating-router",
+        "aurelia-loader-default",
+        "aurelia-history-browser",
+        "aurelia-logging-console",
+        "bootstrap",
+        "bootstrap/css/bootstrap.css!text",
+        "fetch",
+        "jquery"
       ],
-      options: {
-        inject: true,
-        minify: true
+      "options": {
+        "inject": true,
+        "minify": true,
+        "depCache": false,
+        "rev": false
       }
     }
   }
