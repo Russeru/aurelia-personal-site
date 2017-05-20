1e753397aecd1299a70b0a6e7f17657ecf99d967
Walker, Russell E <russell.walker@optum.com>
2017-05-20 17:04:48 -0400

    Add contact page

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ export class App {
     config.map([
       { route: ['', 'home'], name: 'home',      moduleId: 'home',      nav: true, title: 'Home' },
       { route: 'projects',        name: 'projects',        moduleId: 'projects',        nav: true, title: 'Projects' },
-      { route: 'resume', name: 'resume', moduleId: 'resume', nav: true, title: 'Resume'}
+      { route: 'resume', name: 'resume', moduleId: 'resume', nav: true, title: 'Resume'},
+      { route: 'contact', name: 'contact', moduleId: 'contact', nav: true, title: 'Contact'}
     ]);
 
     this.router = router;
