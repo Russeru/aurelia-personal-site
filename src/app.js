db24b7151df7ae224aa5e6dc6b4be0597c0aa213
Walker, Russell E <russell.walker@optum.com>
2017-05-22 11:06:07 -0400

    Make mobile friendly, move contact card to modal

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ export class App {
     config.map([
       { route: ['', 'home'], name: 'home',      moduleId: 'home',      nav: true, title: 'Home' },
       { route: 'projects',        name: 'projects',        moduleId: 'projects',        nav: true, title: 'Projects' },
-      { route: 'experience', name: 'experience', moduleId: 'experience', nav: true, title: 'Experience'},
-      { route: 'contact', name: 'contact', moduleId: 'contact', nav: true, title: 'Contact'}
+      { route: 'experience', name: 'experience', moduleId: 'experience', nav: true, title: 'Experience'}
     ]);
 
     this.router = router;
